6442c652cd2c724531e85580a88ae6b08218f377
sryaerlmba <suryaerlamba3@gmail.com>
2025-07-14 10:56:28 +0700

    add overflow x hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={`${poppins.className} overflow-x-hidden`}>
         <div className="">{children}</div>
       </body>
     </html>
